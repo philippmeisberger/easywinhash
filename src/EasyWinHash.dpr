a64b4519bdfa19f12b3f994959a63622a2c00876
Philipp Meisberger <team@pm-codeworks.de>
2017-10-01 14:16:35 +0200

    Using FastMM4 for leak checking in debug configuration

--- src/EasyWinHash.dpr
+++ src/EasyWinHash.dpr
@@ -9,6 +9,9 @@ uses
 {$R *.res}
 
 begin
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := True;
+{$ENDIF}
   Application.Initialize;
   Application.Title := 'EasyWinHash';
   Application.MainFormOnTaskbar := True;
